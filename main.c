37bf7285e85faf02deb7d9cb0dd3e57e65451c70
Michael Michelotti <50560169+michael-michelotti@users.noreply.github.com>
2024-02-15 15:30:54 -0800

    implement full basic skeleton of sysfs platform driver

--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #include <linux/gpio/consumer.h>
 #include <linux/completion.h>
 #include <linux/mutex.h>
-
+#include <linux/mod_devicetable.h>
+#include <linux/platform_device.h>
 
 dev_t dev_number;
 struct cdev dht11_cdev;
@@ -56,63 +57,44 @@ static const struct attribute_group *dht11_attr_groups[] =
     NULL
 };
 
-static int dht11_open(struct inode *inode, struct file *filep)
+static int dht11_sysfs_probe(struct platform_device *pdev)
+{
+    int ret;
+
+    return 0;
+}
+
+static int dht11_sysfs_remove(struct platform_device *pdev)
 {
     return 0;
 }
 
-static int dht11_release(struct inode *inode, struct file *filep)
+static const struct of_device_id dht11_dt_ids[] = 
 {
-    return 0;
-}
-
-static ssize_t dht11_read(struct file *filep, char __user *buff, size_t count, loff_t *f_pos)
-{
-    return 0;
-}
-
-static ssize_t dht11_write(struct file *filep, const char __user *buff, size_t count, loff_t *f_pos)
-{
-    return 0;
-}
-
-struct file_operations dht11_fops = 
-{
-    .open = dht11_open,
-    .release = dht11_release,
-    .read = dht11_read,
-    .write = dht11_write,
-    .owner = THIS_MODULE
+    { .compatible = "dht11_mm" },
+    { }
 };
+MODULE_DEVICE_TABLE(of, dht11_dt_ids);
 
-struct class *class_dht11;
-struct device *dev_dht11;
+struct platform_driver dht11_sysfs_platform_driver = 
+{
+    .probe = dht11_sysfs_probe,
+    .remove = dht11_sysfs_remove,
+    .driver = {
+        .name = "dht11_mm",
+        .of_match_table = dht11_dt_ids,
+    }
+};
 
 static int dht11_init(void)
 {
-    alloc_chrdev_region(&dev_number, 0, 1, "dht11_devices");
-    pr_info("Device number <major>:<minor> = %d:%d\n", MAJOR(dev_number), MINOR(dev_number));
-
-    cdev_init(&dht11_cdev, &dht11_fops);
-    dht11_cdev.owner = THIS_MODULE;
-    cdev_add(&dht11_cdev, dev_number, 1);
-
-    /* class_create API was updated in kernel version >= 6.4 to have only name, no owner argument.
-    this code will have to be different on host vs. target!!! */
-    class_dht11 = class_create("dht11_class");
-    dev_dht11 = device_create(class_dht11, NULL, dev_number, NULL, "dht11");
-
-    pr_info("Module init successful\n");
+    platform_driver_register(&dht11_sysfs_platform_driver);
     return 0;
 }
 
 static void dht11_deinit(void)
 {
-    device_destroy(class_dht11, dev_number);
-    class_destroy(class_dht11);
-    cdev_del(&dht11_cdev);
-    unregister_chrdev_region(dev_number, 1);
-    pr_info("DHT11 module unloaded.");
+    platform_driver_unregister(&dht11_sysfs_platform_driver);
 }
 
 module_init(dht11_init);
@@ -121,4 +103,3 @@ module_exit(dht11_deinit);
 MODULE_LICENSE("GPL");
 MODULE_AUTHOR("Michael Michelotti <michael.michelotti4@gmail.com>");
 MODULE_DESCRIPTION("DHT11 sensor character driver");
-MODULE_INFO(board, "Beaglebone Black A5");
